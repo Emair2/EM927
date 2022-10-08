acfe3fe6afec79177b427d873e48f25615b7d1b2
Emair2 <yzhu28@saic.edu>
2022-10-07 21:09:53 -0500

    Create Answer.sol

---
+++ em-guess/Answer.sol
@@ -0,0 +1,56 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts@4.7.3/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts@4.7.3/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts@4.7.3/access/Ownable.sol";
+import "@openzeppelin/contracts@4.7.3/token/ERC20/extensions/draft-ERC20Permit.sol";
+
+contract Hash {
+
+
+    // Magic word is "Solidity"
+    function hash(string memory _word) public pure returns (bytes32) {
+        return keccak256(abi.encodePacked(_word));
+    }
+
+}
+
+contract EM is ERC20, ERC20Burnable, Ownable, ERC20Permit {
+    constructor() ERC20("EM", "MTK") ERC20Permit("EM") {
+        _mint(msg.sender, 30000 * 10 ** decimals());
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+
+    function answer1(string memory _guess) public {
+        require(keccak256(abi.encodePacked(_guess)) == 0x7464bd924e765ce487910dde7cf78faee47c96a6328f88a0cd374cd7c2491abd,
+        "Incorrect guess, please try again");
+        _mint(msg.sender, 2 * 10 ** (decimals()-2)); // sends 20000000000000000
+    }
+
+    function answer2(string memory _guess) public {
+        require(keccak256(abi.encodePacked(_guess)) == 0x53cd20a71c8974d8e317a7323d3f45d878d5529670789c7a0ea65b6ff4bf5bf7,
+        "Incorrect guess, please try again");
+        _mint(msg.sender, 2 * 10 ** (decimals()-2)); // sends 20000000000000000
+    }
+
+    function answer3(string memory _guess) public {
+        require(keccak256(abi.encodePacked(_guess)) == 0x5c7d18be0b53c6c839ab2c61a64b224ac3898baced1823dcfd1a0eaa48a1c85e,
+        "Incorrect guess, please try again");
+        _mint(msg.sender, 2 * 10 ** (decimals()-2)); // sends 20000000000000000
+    }
+
+    function answer4(string memory _guess) public {
+        require(keccak256(abi.encodePacked(_guess)) == 0xdf2bb0625b1d8cdbebcdfb13bdd52114aa8fc21207f987594d0baeb8a2ad0616,
+        "Incorrect guess, please try again");
+        _mint(msg.sender, 2 * 10 ** (decimals()-2)); // sends 20000000000000000
+    }
+
+    function answer5(string memory _guess) public {
+        require(keccak256(abi.encodePacked(_guess)) == 0xaaf24dc3fdd0a2e80151f74ae4e840552d17449c8277782edc2c7704237c6cfa,
+        "Incorrect guess, please try again");
+        _mint(msg.sender, 2 * 10 ** (decimals()-2)); // sends 20000000000000000
+    }
