5db4a963b6113adcef6ef4e45755e957752da094
Emair2 <yzhu28@saic.edu>
2022-10-11 13:31:27 -0500

    Update PayFunctionWithToken.sol

--- PayFunctionWithToken.sol
+++ PayFunctionWithToken.sol
@@ -1,11 +1,25 @@
-// my token +
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts@4.7.3/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts@4.7.3/token/ERC20/extensions/ERC20Burnable.sol";
+
+
+contract EM is ERC20, ERC20Burnable {
+    constructor() ERC20("EM", "MTK") {
+        _mint(msg.sender, 30000 * 10 ** decimals());
+    }
+
+    
 
 uint256 public num;
-uint256 public constant FEE = 40;
+uint256 public constant FEE = 10;
 
 function incrementNum() public{
 
     //address recipient, uint256 amount
-    transfer(adress(this), FEE * 10 **decimals());
+    transfer(address(this), FEE * 10 **decimals());
     num++;
+}
+
 }
